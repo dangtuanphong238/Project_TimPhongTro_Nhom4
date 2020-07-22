ae3ea9d0425b313f3012f88da91cf0216a196e81
dangtuanphong238 <dangtuanphong238@github.com>
2020-07-22 23:01:13 +0700

    update Đăng Phòng

--- app/src/main/java/com/example/timphongtro/DangPhongFragment.java
+++ app/src/main/java/com/example/timphongtro/DangPhongFragment.java
@@ -1,15 +1,42 @@
 package com.example.timphongtro;
 
 
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.MimeTypeMap;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.testgooglelogin.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.IOException;
+import java.util.UUID;
+
+import static android.app.Activity.RESULT_OK;
 
 
 /**
@@ -17,17 +44,95 @@ import com.example.testgooglelogin.R;
  */
 public class DangPhongFragment extends Fragment {
 
+    private int PICK_IMAGE_REQUEST = 1;
+    ImageView imgView;
+    Button btnChoose;
+    Button btnDang;
+
+    public Uri mImageUri;
+    private StorageReference mStorageRef;
+    private StorageTask mUploadTask;
+
 
     public DangPhongFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dang_phong, container, false);
+        View fragmentLayout = null;
+        fragmentLayout = inflater.inflate(R.layout.fragment_dang_phong, container, false);
+        btnChoose = fragmentLayout.findViewById(R.id.btnChoose);
+        btnDang = fragmentLayout.findViewById(R.id.btnDang);
+        imgView = fragmentLayout.findViewById(R.id.imgView);
+
+        mStorageRef = FirebaseStorage.getInstance().getReference("uploads");
+
+
+        btnChoose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chooseImage();
+            }
+        });
+
+        btnDang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    uploadImage();
+            }
+        });
+
+        return fragmentLayout;
     }
 
+    private String getFileExtension(Uri uri) {
+        ContentResolver cR = getContext().getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(cR.getType(uri));
+    }
+    private void uploadImage() {
+
+        StorageReference Ref = mStorageRef.child(System.currentTimeMillis()+"."+getFileExtension(mImageUri));
+        Ref.putFile(mImageUri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        // Get a URL to the uploaded content
+//                        Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                        Toast.makeText(getActivity().getApplication(), "Completed", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        // Handle unsuccessful uploads
+                        // ...
+                    }
+                });
+    }
+
+
+    private void chooseImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent,PICK_IMAGE_REQUEST);
+
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+                && data != null && data.getData() != null )
+        {
+            mImageUri = data.getData();
+            imgView.setImageURI(mImageUri);
+
+        }
+    }
 }
