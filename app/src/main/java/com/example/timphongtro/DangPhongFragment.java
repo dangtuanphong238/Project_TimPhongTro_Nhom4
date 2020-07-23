cc3a2aaadd3d35e4863b5f0f400167cffca201c6
dangtuanphong238 <dangtuanphong238@github.com>
2020-07-23 10:15:18 +0700

    update
    Update Chức năng Đăng Phòng

--- app/src/main/java/com/example/timphongtro/DangPhongFragment.java
+++ app/src/main/java/com/example/timphongtro/DangPhongFragment.java
@@ -1,40 +1,27 @@
 package com.example.timphongtro;
 
-
-import android.app.Activity;
-import android.app.ProgressDialog;
-import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-
 import android.provider.MediaStore;
-import android.util.Log;
+import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.MimeTypeMap;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.RadioButton;
 import android.widget.Toast;
-
 import com.example.testgooglelogin.R;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.OnProgressListener;
-import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.StorageTask;
-import com.google.firebase.storage.UploadTask;
-
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.util.UUID;
 
 import static android.app.Activity.RESULT_OK;
 
@@ -46,12 +33,14 @@ public class DangPhongFragment extends Fragment {
 
     private int PICK_IMAGE_REQUEST = 1;
     ImageView imgView;
-    Button btnChoose;
+    ImageButton btnChoose;
     Button btnDang;
+    RadioButton rdKTX,rdPhongChoThue,rdPhongOGhep, rdNhaNguyenCan, rdTatCa, rdNam, rdNu;
+    EditText edtSucChua, edtDienTich, edtChiPhi, edtDiaChi;
+
 
     public Uri mImageUri;
-    private StorageReference mStorageRef;
-    private StorageTask mUploadTask;
+    Bitmap selectedBitmap;
 
 
     public DangPhongFragment() {
@@ -59,18 +48,27 @@ public class DangPhongFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(final LayoutInflater inflater, final ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View fragmentLayout = null;
         fragmentLayout = inflater.inflate(R.layout.fragment_dang_phong, container, false);
+
+        //Ánh xạ view
         btnChoose = fragmentLayout.findViewById(R.id.btnChoose);
         btnDang = fragmentLayout.findViewById(R.id.btnDang);
         imgView = fragmentLayout.findViewById(R.id.imgView);
-
-        mStorageRef = FirebaseStorage.getInstance().getReference("uploads");
-
-
+        rdKTX = fragmentLayout.findViewById(R.id.rdKTX);
+        rdPhongChoThue = fragmentLayout.findViewById(R.id.rdPhongChoThue);
+        rdPhongOGhep = fragmentLayout.findViewById(R.id.rdPhongOGhep);
+        rdNhaNguyenCan = fragmentLayout.findViewById(R.id.rdNhaNguyenCan);
+        edtChiPhi = fragmentLayout.findViewById(R.id.edtChiPhi);
+        edtDienTich = fragmentLayout.findViewById(R.id.edtDienTich);
+        edtSucChua = fragmentLayout.findViewById(R.id.edtSucChua);
+        rdTatCa = fragmentLayout.findViewById(R.id.rdTatCa);
+        rdNam = fragmentLayout.findViewById(R.id.rdNam);
+        rdNu = fragmentLayout.findViewById(R.id.rdNu);
+        edtDiaChi = fragmentLayout.findViewById(R.id.edtDiaChi);
         btnChoose.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -81,38 +79,83 @@ public class DangPhongFragment extends Fragment {
         btnDang.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                    uploadImage();
+                    xuLyThemMoi();
+                    startActivity(Intent.makeMainActivity(getActivity().getComponentName()));
+
             }
         });
 
         return fragmentLayout;
     }
 
-    private String getFileExtension(Uri uri) {
-        ContentResolver cR = getContext().getContentResolver();
-        MimeTypeMap mime = MimeTypeMap.getSingleton();
-        return mime.getExtensionFromMimeType(cR.getType(uri));
-    }
-    private void uploadImage() {
+//    private String getFileExtension(Uri uri) {
+//        ContentResolver cR = getContext().getContentResolver();
+//        MimeTypeMap mime = MimeTypeMap.getSingleton();
+//        return mime.getExtensionFromMimeType(cR.getType(uri));
+//    }
+//    private void uploadImage() {
+//
+//        StorageReference Ref = mStorageRef.child(System.currentTimeMillis()+"."+getFileExtension(mImageUri));
+//        Ref.putFile(mImageUri)
+//                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+//                    @Override
+//                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+//                        // Get a URL to the uploaded content
+////                        Uri downloadUrl = taskSnapshot.getDownloadUrl();
+//                        Toast.makeText(getActivity().getApplication(), "Completed", Toast.LENGTH_SHORT).show();
+//                    }
+//                })
+//                .addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception exception) {
+//                        // Handle unsuccessful uploads
+//                        // ...
+//                    }
+//                });
+//    }
+//
+public void xuLyThemMoi() {
 
-        StorageReference Ref = mStorageRef.child(System.currentTimeMillis()+"."+getFileExtension(mImageUri));
-        Ref.putFile(mImageUri)
-                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                    @Override
-                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                        // Get a URL to the uploaded content
-//                        Uri downloadUrl = taskSnapshot.getDownloadUrl();
-                        Toast.makeText(getActivity().getApplication(), "Completed", Toast.LENGTH_SHORT).show();
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception exception) {
-                        // Handle unsuccessful uploads
-                        // ...
-                    }
-                });
+    //get data Loai phòng
+    String loaiPhong= "";
+    if(rdKTX.isChecked()) loaiPhong = rdKTX.getText().toString();
+    else if(rdPhongChoThue.isChecked()) loaiPhong = rdPhongChoThue.getText().toString();
+    else if(rdPhongOGhep.isChecked()) loaiPhong = rdPhongOGhep.getText().toString();
+    else if(rdNhaNguyenCan.isChecked()) loaiPhong = rdNhaNguyenCan.getText().toString();
+
+    //get data Giới tính
+    String gioiTinh= "";
+    if(rdTatCa.isChecked()) gioiTinh = rdTatCa.getText().toString();
+    else if(rdNam.isChecked()) gioiTinh = rdNam.getText().toString();
+    else if(rdNu.isChecked()) gioiTinh = rdNu.getText().toString();
+
+    double dienTich = Double.parseDouble(edtDienTich.getText().toString());
+    double chiPhi = Double.parseDouble(edtChiPhi.getText().toString());
+    int sucChua = Integer.parseInt(edtSucChua.getText().toString());
+    String diaChi = edtDiaChi.getText().toString();
+    try {
+        DatabaseReference databaseRef = FirebaseDatabase.getInstance().getReference().child("RoomA");
+        //Kết nối tới node có tên là contacts (node này do ta định nghĩa trong CSDL Firebase)
+        String key = databaseRef.push().getKey();
+        databaseRef.child(key).child("loaiphong").setValue(loaiPhong);
+        databaseRef.child(key).child("succhua").setValue(sucChua);
+        databaseRef.child(key).child("gioitinh").setValue(gioiTinh);
+        databaseRef.child(key).child("dientich").setValue(dienTich);
+        databaseRef.child(key).child("chiphi").setValue(chiPhi);
+        databaseRef.child(key).child("diachi").setValue(diaChi);
+
+//đưa bitmap về base64string:
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        selectedBitmap.compress(Bitmap.CompressFormat.PNG, PICK_IMAGE_REQUEST, byteArrayOutputStream);
+        byte[] byteArray = byteArrayOutputStream.toByteArray();
+        String imgeEncoded = Base64.encodeToString(byteArray, Base64.DEFAULT);
+        databaseRef.child(key).child("picture").setValue(imgeEncoded);
+        getActivity().finish();
+
+    } catch (Exception ex) {
+        Toast.makeText(getActivity(), "Error:" + ex.toString(), Toast.LENGTH_LONG).show();
     }
+}
 
 
     private void chooseImage() {
@@ -130,8 +173,14 @@ public class DangPhongFragment extends Fragment {
         if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
                 && data != null && data.getData() != null )
         {
-            mImageUri = data.getData();
-            imgView.setImageURI(mImageUri);
+            try {
+                mImageUri = data.getData();
+                selectedBitmap = MediaStore.Images.Media.getBitmap(getActivity().getApplicationContext().getContentResolver(), mImageUri);
+                imgView.setImageBitmap(selectedBitmap);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
         }
     }
