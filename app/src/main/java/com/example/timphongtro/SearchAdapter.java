21b06ecb491f0b8c511f8ae2cb5a4fc92436d443
hophantennhan2 <67866878+hophantennhan2@users.noreply.github.com>
2020-08-04 17:00:22 +0700

    ThanhNhan Update lastday
    Update2.2

--- app/src/main/java/com/example/timphongtro/SearchAdapter.java
+++ app/src/main/java/com/example/timphongtro/SearchAdapter.java
@@ -2,16 +2,20 @@ package com.example.timphongtro;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.util.Base64;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.testgooglelogin.R;
@@ -22,6 +26,8 @@ public class SearchAdapter extends RecyclerView.Adapter<SearchAdapter.MyViewHold
 
     Context context;
     ArrayList<SearchModel> arrayList;
+    CardView cardviewSearch;
+    RecyclerView recyclerView;
     public SearchAdapter(Context c, ArrayList<SearchModel> arr)
     {
         context = c;
@@ -41,13 +47,37 @@ public class SearchAdapter extends RecyclerView.Adapter<SearchAdapter.MyViewHold
             txtGioiTinh = itemView.findViewById( R.id.txtGioiTinh);
             imgSearch = itemView.findViewById( R.id.imgSearch);
             txtSucChua = itemView.findViewById( R.id.txtSucChua);
-
+            recyclerView = itemView.findViewById( R.id.myRecyclerSearch );
+            cardviewSearch = itemView.findViewById( R.id.cardviewSearch );
         }
     }
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return new MyViewHolder(LayoutInflater.from(context).inflate( R.layout.cardviewsearch, parent,false));
+        final View view = LayoutInflater.from( context).inflate( R.layout.cardviewsearch ,parent,false);
+        view.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent( context, InfoSearch.class );
+                int pos = (int) v.getTag();
+
+                intent.putExtra( "chiphi",arrayList.get(pos).getChiphi() );
+                intent.putExtra( "diachi",arrayList.get( pos ).getDiachi() );
+                intent.putExtra( "dientich",arrayList.get(pos).getDientich() );
+                intent.putExtra( "gioitinh",arrayList.get( pos ).getGioitinh() );
+                intent.putExtra( "loaiphong",arrayList.get(pos).getLoaiphong() );
+                intent.putExtra( "succhua",arrayList.get( pos ).getSucchua() );
+                if(arrayList.get( pos ).getPicture()!= null){
+                    byte[] decodedString = Base64.decode( arrayList.get(pos).getPicture(),Base64.DEFAULT );
+                    intent.putExtra( "decoded",decodedString );
+                }
+                context.startActivity(intent);
+            }
+
+        } );
+        return new MyViewHolder(view);
+
+
     }
 
     @SuppressLint("SetTextI18n")
@@ -64,6 +94,7 @@ public class SearchAdapter extends RecyclerView.Adapter<SearchAdapter.MyViewHold
             holder.imgSearch.setImageBitmap(decodedByte);
         }
         holder.txtSucChua.setText("Sức Chứa: " + arrayList.get(position).getSucchua());
+        cardviewSearch.setTag(position);
 
     }
 
